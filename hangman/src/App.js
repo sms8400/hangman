4849b6f9469f4bc2039222cd46ba3048062cbf19
Sue <138933788+sms8400@users.noreply.github.com>
2023-10-04 13:38:30 -0400

    complete

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -9,7 +9,7 @@ import { showNotification as show, checkWin } from './helpers/helpers';
 
 import './App.css';
 
-const words = ['application', 'programming', 'interface', 'wizard'];
+const words = ['application', 'programming', 'interface', 'wizard', 'Abundance', 'Bold', 'Challenge', 'Zebra', 'Xray', 'Thrive', 'JavaScript', 'React', 'Java', 'Python', 'Persistance' ];
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
